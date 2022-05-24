b740951a97783a839fa0710d6b9e0950f5324e64
Darius Neațu <neatudarius@gmail.com>
2022-05-24 18:54:37 +0300

    2022: new max flow skel

--- skel/lab12/java/task-1/src/Main.java
+++ skel/lab12/java/task-1/src/Main.java
@@ -4,9 +4,14 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Scanner;
+import java.lang.Math;
+import java.text.CollationElementIterator;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
 public class Main {
     static class Task {
@@ -16,6 +21,9 @@ public class Main {
         // numarul maxim de noduri
         public static final int NMAX = 1005;
 
+        // valoare mai mare decat maxFlow
+        public static final int INF = (int) 10e9;
+
         // n = numar de noduri, m = numar de muchii
         int n, m;
 
@@ -24,7 +32,30 @@ public class Main {
         ArrayList<Integer> adj[] = new ArrayList[NMAX];
 
         // cap[i][j] = capacitatea arcului i -> j
-        int cap[][];
+        int c[][];
+
+        public class Edge {
+            public int node;
+            public int neigh;
+
+            Edge(int _node, int _neigh) {
+                node = _node;
+                neigh = _neigh;
+            }
+
+        }
+
+        // structura folosita pentru a stoca daca exista drum de ameliorare
+        // si care este acesta.
+        public class AugmentedPath {
+            boolean hasPath;
+            ArrayList<Edge> path;
+
+            AugmentedPath(boolean _hasPath, ArrayList<Edge> _path) {
+                hasPath = _hasPath;
+                path = _path;
+            }
+        }
 
         public void solve() {
             readInput();
@@ -34,27 +65,29 @@ public class Main {
         private void readInput() {
             try {
                 Scanner sc = new Scanner(new BufferedReader(new FileReader(
-                                INPUT_FILE)));
+                        INPUT_FILE)));
                 n = sc.nextInt();
                 m = sc.nextInt();
 
-                cap = new int[n + 1][n + 1];
                 for (int i = 1; i <= n; i++) {
                     adj[i] = new ArrayList<>();
                 }
+
+                c = new int[n + 1][n + 1];
+
                 for (int i = 1; i <= m; i++) {
                     // x -> y de capacitate c
-                    int x, y, c;
-                    x = sc.nextInt();
-                    y = sc.nextInt();
-                    c = sc.nextInt();
-                    adj[x].add(y);
-                    adj[y].add(x);
+                    int u, v, capacity;
+                    u = sc.nextInt();
+                    v = sc.nextInt();
+                    capacity = sc.nextInt();
+                    adj[u].add(v);
+                    adj[v].add(u);
 
                     // Presupunem existenta mai multor arce x -> y cu capacitati c1, c2, ...
                     // Comprimam intr-un singur arc x -> y cu capacitate
                     // cap[x][y] = c1 + c2 + ...
-                    cap[x][y] += c;
+                    c[u][v] += capacity;
                 }
                 sc.close();
             } catch (IOException e) {
@@ -65,7 +98,7 @@ public class Main {
         private void writeOutput(int result) {
             try {
                 PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(
-                                OUTPUT_FILE)));
+                        OUTPUT_FILE)));
                 pw.printf("%d\n", result);
                 pw.close();
             } catch (IOException e) {
@@ -80,16 +113,15 @@ public class Main {
             // Destinatia este nodul n.
             //
             // In adj este stocat graful neorientat obtinut dupa ce se elimina orientarea
-            // arcelor, iar in cap sunt stocate capacitatile arcelor.
-            // De exemplu, un arc (x, y) de capacitate z va fi tinut astfel:
-            // cap[x][y] = z, adj[x] contine y, adj[y] contine x.
+            // arcelor, iar in c sunt stocate capacitatile arcelor.
+            // De exemplu, un arc (x, y) de capacitate cap va fi tinut astfel:
+            // c[x][y] = cap, adj[x] contine y, adj[y] contine x.
             //
-            int maxFlow = 0;
-
-            return maxFlow;
+            int totalFlow = 0;
+            return totalFlow;
         }
-    }
 
+    }
     public static void main(String[] args) {
         new Task().solve();
     }
