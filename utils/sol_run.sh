4c00402e50a5bc234e16814df04b581b240a9e0f
Radu Nichita <radunichita99@gmail.com>
2025-03-09 14:16:18 +0200

    Add workflow for running sol

--- utils/sol_run.sh
+++ utils/sol_run.sh
@@ -3,8 +3,8 @@
 
 set -e
 
-if (($# < 3)); then
-    echo "Usage: ${0} <lab> <lang> <sol-name>"
+if (($# < 4)); then
+    echo "Usage: ${0} <lab> <lang> <sol-name> <task-name>"
     exit 1
 fi
 
@@ -12,7 +12,7 @@ LAB="${1}"
 LANG="${2}"
 
 SOL_NAME="${3}"
-TASK_NAME=${4}
+TASK_NAME="${4}"
 
 WORKING_DIR="/tmp/mumu"
 rm -rf "${WORKING_DIR}" 2>&1 1>/dev/null
@@ -24,7 +24,7 @@ SKEL_EXPORT_SCRIPT="$(dirname ${0})/skel_export.sh"
 
 # copy sol
 SOL_EXPORT_SCRIPT="$(dirname ${0})/sol_export.sh"
-"${SOL_EXPORT_SCRIPT}" "${LAB}" "${LANG}" "${SOL_NAME}" "${WORKING_DIR}" 2>&1 1>/dev/null
+"${SOL_EXPORT_SCRIPT}" "${LAB}" "${LANG}" "${TASK_NAME}" "${SOL_NAME}.${LANG}" "${WORKING_DIR}" 2>&1 1>/dev/null
 
 pushd "${WORKING_DIR}/skel/${LAB}" 2>&1 1>/dev/null
 
--- utils/sol_run_all.sh
+++ utils/sol_run_all.sh
@@ -3,54 +3,46 @@
 
 set -e
 
+UTILS_PATH="$(realpath $(dirname ${0}))"
+
 error() {
     echo "$1"
     exit 1
 }
 
 declare -a solutions=(
-    "lab01,cpp,sol_2020_dn"
-    "lab01,java,sol_2020_dn"
-    "lab02,cpp,sol_2020_dn"
-    "lab02,cpp,sol_2020_dn2,task-3"
-    "lab02,java,sol_2020_dn"
-    "lab03,cpp,sol_2020_dn"
-    "lab03,java,sol_2020_sp"
-    "lab04,cpp,sol_2020_dn"
-    "lab04,cpp,sol_2020_dn2,task-1"
-    "lab04,cpp,sol_2020_dn3,task-1"
-    "lab04,cpp,sol_2020_dn4,task-1"
-    "lab04,java,sol_2020_sp"
-    "lab05,cpp,sol_2020_dn"
-    "lab05,java,sol_2020_sp"
-    "lab07,cpp,sol_2020_dn"
-    "lab07,cpp,sol_2020_dn2,task-2"
-    "lab07,java,sol_2020_sp"
-    "lab07,java,sol_2020_sp2,task-2"
-    "lab08,cpp,sol_2020_dn"
-    "lab08,cpp,sol_2020_dn2,task-1"
-    "lab08,cpp,sol_2021_rn,task-4"
-    "lab08,java,sol_2021_rn"
-    "lab08,java,sol_2020_sp,task-1"
-    "lab08,java,sol_2020_sp,task-2"
-    "lab08,java,sol_2020_sp,task-3"
-    "lab09,cpp,sol_2022_rn"
-    "lab09,java,sol_2022_rn"
-    "lab10,cpp,sol_2022_rn"
-    "lab10,java,sol_2022_rn"
-    "lab11,cpp,sol_2022_dn"
-    "lab11,java,sol_2022_dn"
-    "lab12,cpp,sol_2022_dn"
-    "lab12,java,sol_2022_dn"
+    "lab01,cpp"
+    "lab01,java"
 )
 
 for solution in "${solutions[@]}"; do
-    TASK_NAME=
-    IFS=, read LAB LANG SOL_NAME TASK_NAME <<<"${solution}"
+    TASK_NAME=""
+    IFS=, read LAB LANG <<<"${solution}"
 
-    printf "running sol/%s: %s/%s %s ...\n" "${LAB}" "${LANG}" "${SOL_NAME}" "${TASK_NAME}"
-    ./utils/sol_run.sh "${LAB}" "${LANG}" "${SOL_NAME}" "${TASK_NAME}" || error "${LAB}/${LANG}/${SOL_NAME}/${TASK_NAME} failed!"
-    printf "\n\n\n"
+    SOL_PATH="$(realpath ${UTILS_PATH}/..)/sol/${LAB}"
+
+    if [ ! -d "$SOL_PATH" ]; then
+        echo "Solution folder for ${LAB} does not exist!"
+        exit 1
+    fi
+
+    for task in ${SOL_PATH}/${LANG}/*; do
+        SOL_PATH=$task
+        if [ -e "${task}/src" ]; then
+            SOL_PATH=$task/src
+        fi
+
+        if [ -d "$task" ]; then
+            TASK_NAME=$(basename "$task")
+            for sol in $SOL_PATH/*; do
+                SOL_FILE=$(basename "$sol") 
+                SOL_NAME="${SOL_FILE%.*}"
+                printf "running sol/%s: %s/%s %s ...\n" "${LAB}" "${LANG}" "${SOL_NAME}" "${TASK_NAME}"
+                ./utils/sol_run.sh "${LAB}" "${LANG}" "${SOL_NAME}" "${TASK_NAME}" || error "${LAB}/${LANG}/${SOL_NAME}/${TASK_NAME} failed!"
+                printf "\n\n\n"
+            done
+        fi
+    done
 done
 
 echo "sol/* passed!"
