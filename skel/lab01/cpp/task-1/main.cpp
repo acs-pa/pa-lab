5fd173f6cea915c9ce3f53edecd226a4078bbadd
Radu Nichita <radunichita99@gmail.com>
2025-03-09 11:16:21 +0200

    labs: Add license for source files

--- skel/lab01/cpp/task-1/main.cpp
+++ skel/lab01/cpp/task-1/main.cpp
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: BSD-3-Clause
+
 #include <iostream>         // cin, cout
 #include <fstream>          // ifstream, ofstream
 #include <vector>           // vector
--- skel/lab04/java/task-2/src/Main.java
+++ skel/lab04/java/task-2/src/Main.java
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: BSD-3-Clause
+
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
