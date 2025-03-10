c2fa672bcfeab0fecda934badcb5ef0470a98f78
Darius Neatu <dariusn@adobe.com>
2025-03-10 10:55:09 +0200

    Fix broken Python lint

--- utils/lint_all.sh
+++ utils/lint_all.sh
@@ -2,13 +2,13 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 # Lint C++.
-find . -name "*.cpp" -or -name "*.h" | xargs clang-format -i
+find . -name "*.cpp" -or -name "*.cxx" -or -name "*.h" -or -name "*.hpp" | xargs clang-format -i
 
 # Lint Java.
 # TODO
 
 # Lint Python.
-# TODO
+find . -name "*.py" -or -name "check_task" | xargs autopep8 -i
 
 # Lint Shell.
 # TODO
